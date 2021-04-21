609d54b932bb26e058b918f0b4ca233bd8b70720
darwin chu <darwinchu12@gmail.com>
2021-04-21 23:05:51 +0800

    Added Role to Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
